c1dc23f454e63f4e5ad413449f408c458a572372
Luis Alejandro Cruz Inzunza <cruzalejandro113@gmail.com>
2023-01-14 16:53:35 -0600

    Yo hice todo -Luis

--- disenoT2/src/main/java/org/example/Main.java
+++ disenoT2/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.io.*;
+
+import javax.swing.JOptionPane;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Hola mundo");
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+  
+        try {
+           archivo = new File ("disenoT2/src/CSV/Alumnos.csv");
+           fr = new FileReader (archivo);
+           br = new BufferedReader(fr);
+  
+           // Lectura de cada linea
+           String linea;
+           while((linea=br.readLine())!=null){
+            System.out.println(linea);
+
+
+           }
+        }
+        catch(Exception e){
+           e.printStackTrace();
+        }
+        
     }
 }
