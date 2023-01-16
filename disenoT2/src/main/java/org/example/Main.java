2c89b12ea61be6962e34611e19f2909cbb0dc348
Luis Alejandro Cruz Inzunza <cruzalejandro113@gmail.com>
2023-01-16 11:54:45 -0600

    yo lo hice todo Luis (lectura csv y arreglo bidimensional problem1)

--- disenoT2/src/main/java/org/example/Main.java
+++ disenoT2/src/main/java/org/example/Main.java
@@ -7,27 +7,7 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        JOptionPane.showMessageDialog(null, "Hola mundo");
-        File archivo = null;
-        FileReader fr = null;
-        BufferedReader br = null;
-  
-        try {
-           archivo = new File ("disenoT2/src/CSV/Alumnos.csv");
-           fr = new FileReader (archivo);
-           br = new BufferedReader(fr);
-  
-           // Lectura de cada linea
-           String linea;
-           while((linea=br.readLine())!=null){
-            System.out.println(linea);
-
-
-           }
-        }
-        catch(Exception e){
-           e.printStackTrace();
-        }
-        
+        LecturaCSV archivo = new LecturaCSV("disenoT2/src/CSV/Alumnos.csv");
+        archivo.Leer();
     }
 }
